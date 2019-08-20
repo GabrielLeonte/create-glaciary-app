39c4e5d9c0a927b23a3db8c905a188b63e744127
Gabriel Leonte <38123199+GabrielLeonte@users.noreply.github.com>
2019-08-20 14:42:54 +0300

    Initial commit

---
+++ cli.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk')
+const cli = require('cac')('create-nuxt-app')
+const cliparse = cli.parse()
+const name = cliparse.args[0]
+const cwd = process.cwd()
+
+
+// Display help message when `-h` or `--help` appears.
+cli.help();
+
+
+// Display version number when `-v` or `--version` appears.
+cli.version('1.0.0');
+
+
+// Run createApp function
+require('./createApp').handle(name, cwd, chalk)
+
+
---
+++ createApp.js
@@ -0,0 +1,54 @@
+const fs = require('fs-extra');
+const utils = require('./utils');
+
+
+exports.handle = function (name, cwd, chalk) {
+    if (name) {
+        if (name.match(/[A-Z\W]/) == null) {
+            console.clear();
+            utils.print(`Creating a new Glaciary.JS project in ${chalk.green(cwd + `\\${name}`)}\n\n`);
+            createApp(cwd, name);
+        } else {
+            utils.print(`Upper cases and special characters are not supported`);
+        }
+    } else {
+        utils.print(`You have to enter a name for your app. \nUse ${chalk.green("create-glaciary-app name")}, where name is the name of your app.`);
+    }
+}
+
+
+function createApp(cwd, name) {
+    let dir = cwd + `\\${name}`;
+
+    utils.print(`Creating app folder...`);
+
+    if (createAppDir(dir)) {
+
+        utils.print('Successfully created the app folder');
+
+        if (copyFiles(dir)) {
+            utils.print(`App project files successfully placed in ${dir}`);
+        }
+
+    }
+}
+
+function createAppDir(dir) {
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+        return true;
+    }
+
+}
+
+function copyFiles(dir) {
+    fs.copy(__dirname + '/template', dir, err => {
+        if (err) {
+            return console.error(err);
+        }
+        else {
+            return true;
+        }
+    });
+}
+
